21079494e5f4184ec37fb1df5eb44f3176adb8a7
HusnainMughal338 <mughalhusnain437@gmail.com>
2024-08-12 06:46:05 +0000

    init github

---
+++ index.py
@@ -0,0 +1,13 @@
+print("hello word")
+num1=55
+num2=98
+sum=num1+num2
+print(sum)
+product=21*22
+print(product)
+division=22//2
+print("division",division)
+differ=22-23
+print(differ)
+product=4**10
+print("product",product)
